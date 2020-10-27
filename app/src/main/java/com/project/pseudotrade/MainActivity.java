cdb1425aa50a6bc2beb15299dee0ccd4e3179c5e
amberdsilva <dsil9200@mylaurier.ca>
2020-10-27 19:11:50 -0400

    main page - amber and ki

--- app/src/main/java/com/project/pseudotrade/MainActivity.java
+++ app/src/main/java/com/project/pseudotrade/MainActivity.java
@@ -1,8 +1,12 @@
 package com.project.pseudotrade;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        TabLayout tabMain = findViewById(R.id.Main_page);
+        TabLayout tabStock = findViewById(R.id.Stocks_page);
+        TabLayout tabSetting = findViewById(R.id.Settings_page);
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        PagerAdapter pagerAdapter = new PageAdapter(getSupportFragmentManager(),
+                tabLayout.getTabCount());
+
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem((tab.getPosition()));
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
+
+
 }
