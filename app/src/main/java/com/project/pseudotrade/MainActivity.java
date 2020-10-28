9f75554b2f0449e7f4c80792d063dcb22403ab8d
Basil Zuberi <basilzuberi@hotmail.com>
2020-10-28 18:47:07 -0400

    newest iteration of app (no fragment tab layout)

--- app/src/main/java/com/project/pseudotrade/MainActivity.java
+++ app/src/main/java/com/project/pseudotrade/MainActivity.java
@@ -4,44 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mBtnSettingsPage;
+    Button mBtnStocksPage;
+    Button mBtnMainPage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabLayout tabLayout = findViewById(R.id.tabLayout);
-        TabLayout tabMain = findViewById(R.id.Main_page);
-        TabLayout tabStock = findViewById(R.id.Stocks_page);
-        TabLayout tabSetting = findViewById(R.id.Settings_page);
-        ViewPager viewPager = findViewById(R.id.viewPager);
+        mBtnSettingsPage = findViewById(R.id.Settings_page);
+        mBtnStocksPage = findViewById(R.id.Stock_page);
 
-        PagerAdapter pagerAdapter = new PageAdapter(getSupportFragmentManager(),
-                tabLayout.getTabCount());
+//        TabLayout tabLayout = findViewById(R.id.tabLayout);
+//        TabLayout tabMain = findViewById(R.id.Main_page);
+//        TabLayout tabStock = findViewById(R.id.Stocks_page);
+//        TabLayout tabSetting = findViewById(R.id.Settings_page);
+//        ViewPager viewPager = findViewById(R.id.viewPager);
 
-        viewPager.setAdapter(pagerAdapter);
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        mBtnStocksPage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem((tab.getPosition()));
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
+            public void onClick(View v) {
+                Intent stocksIntent = new Intent(MainActivity.this, StocksActivity.class);
+                startActivity(stocksIntent);
             }
         });
+
+        mBtnSettingsPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(settingsIntent);
+            }
+        });
+
+
+
     }
 
 
