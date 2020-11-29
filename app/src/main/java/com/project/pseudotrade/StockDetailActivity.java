1d1416e2881b1986eb81d207156ab18d06d92c5c
Patrick Mandarino <mand6560@mylaurier.ca>
2020-11-29 13:39:02 -0500

    Stocks page now shows company names, now using Dumb Stock API, getting ready for stock detail page

---
+++ app/src/main/java/com/project/pseudotrade/StockDetailActivity.java
@@ -0,0 +1,14 @@
+package com.project.pseudotrade;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StockDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock_detail);
+    }
+}
--- app/src/main/java/com/project/pseudotrade/StocksActivity.java
+++ app/src/main/java/com/project/pseudotrade/StocksActivity.java
@@ -27,6 +27,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -34,6 +35,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -56,6 +58,7 @@ public class StocksActivity extends AppCompatActivity {
     Double cashBalance;
 
     ArrayList<String> stockSymbols;
+    ArrayList<String> stockNames;
     ArrayList<Double> stockPrices;
 
     private static final String ACTIVITY_NAME = "StocksActivity";
@@ -84,9 +87,10 @@ public class StocksActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Stock stock = stockListAdapter.getItem(position);
-                String tempMessage = String.format("A detailed page for %s will be shown eventually...", stock.getName());
+                /*String tempMessage = String.format("A detailed page for %s will be shown eventually...", stock.getName());
                 Toast toast = Toast.makeText(StocksActivity.this, tempMessage, Toast.LENGTH_SHORT);
-                toast.show();
+                toast.show();*/
+
             }
         });
 
@@ -95,6 +99,7 @@ public class StocksActivity extends AppCompatActivity {
             userID = userDataBundle.getString("userID");
         holdings = new HashMap<>();
         stockSymbols = new ArrayList<>();
+        stockNames = new ArrayList<>();
 
         mDatabase = FirebaseDatabase.getInstance(); // Get current user's balance and portfolio holdings
         mDatabaseReference = mDatabase.getReference("Users");
@@ -177,6 +182,14 @@ public class StocksActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 prices.add(price);
+
+                String name = "";
+                try {
+                    name = getName(symbol);
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+                stockNames.add(name);
             }
 
             return prices;
@@ -186,7 +199,7 @@ public class StocksActivity extends AppCompatActivity {
         protected void onPostExecute(ArrayList<Double> doubles) {
             stockPrices = doubles;
             refreshProgressBar.setVisibility(View.INVISIBLE);
-            populateStockList(stockSymbols, stockPrices);
+            populateStockList(stockSymbols, stockNames, stockPrices);
         }
 
         @Override
@@ -219,9 +232,31 @@ public class StocksActivity extends AppCompatActivity {
                 return latestPrice;
             }
         }
+
+        private String getName(String symbol) throws IOException, JSONException {
+            URL dumbStockURL = new URL("https://dumbstockapi.com/stock?ticker_search=" + symbol);
+            HttpURLConnection connection = (HttpURLConnection)dumbStockURL.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            int response = connection.getResponseCode();
+            if (response != 200)
+                throw new RuntimeException("HttpResponseCode: " + response);
+            else {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuffer result = new StringBuffer();
+                while ((inputLine = in.readLine()) != null)
+                    result.append(inputLine);
+                connection.disconnect();
+                in.close();
+                JSONObject jsonResult = new JSONObject(new JSONArray(result.toString()).getString(0));
+                String name = jsonResult.getString("name");
+                return name;
+            }
+        }
     }
 
-    private void populateStockList(ArrayList<String> symbols, ArrayList<Double> prices) {
+    private void populateStockList(ArrayList<String> symbols, ArrayList<String> names, ArrayList<Double> prices) {
         if (prices.contains(null)) {
             Toast errorToast = Toast.makeText(this, "Portfolio cannot be refreshed at this time. Please try again in a minute.", Toast.LENGTH_SHORT);
             errorToast.show();
@@ -230,7 +265,7 @@ public class StocksActivity extends AppCompatActivity {
             stockListAdapter.notifyDataSetChanged();
 
             for (int i = 0; i < symbols.size(); i++) {
-                Stock stock = new Stock(symbols.get(i), holdings.get(symbols.get(i)), prices.get(i));
+                Stock stock = new Stock(symbols.get(i), names.get(i), holdings.get(symbols.get(i)), prices.get(i));
                 stockList.add(stock);
             }
             stockListAdapter.notifyDataSetChanged();
@@ -254,7 +289,8 @@ public class StocksActivity extends AppCompatActivity {
             LayoutInflater inflater = StocksActivity.this.getLayoutInflater();
             View result = inflater.inflate(R.layout.stock_row, null);
             TextView stockRowName = (TextView) result.findViewById(R.id.stock_name);
-            stockRowName.setText(getItem(position).getName());
+            //stockRowName.setText(getItem(position).getTicker());
+            stockRowName.setText(String.format("%s\n(%s)", getItem(position).getTicker(), getItem(position).getName()));
             TextView stockRowHoldingsValue = (TextView) result.findViewById(R.id.holdings_value);
             String stockValue = String.format("$%.02f", getItem(position).getHoldingsValue());
             stockRowHoldingsValue.setText(stockValue);
@@ -269,17 +305,21 @@ public class StocksActivity extends AppCompatActivity {
     }
 
     private class Stock {
+        private final String ticker;
         private final String name;
         private final int quantity;
         private final double currentPrice;
         private final double holdingsValue;
 
-        public Stock(String name, int quantity, double currentPrice) {
+        public Stock(String ticker, String name, int quantity, double currentPrice) {
+            this.ticker = ticker;
             this.name = name;
             this.quantity = quantity;
             this.currentPrice = currentPrice;
             this.holdingsValue = this.quantity * this.currentPrice;
         }
+
+        public String getTicker() { return this.ticker; }
 
         public String getName() { return this.name; }
 
