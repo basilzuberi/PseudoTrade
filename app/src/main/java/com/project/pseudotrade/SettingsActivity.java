8be655f2365b2d3d8260f7b2bdcab786fc44fa9c
SirHappi <sri13.vinukonda@gmail.com>
2020-12-01 19:24:17 -0500

    Balance updates EVERYTIME now

--- app/src/main/java/com/project/pseudotrade/SettingsActivity.java
+++ app/src/main/java/com/project/pseudotrade/SettingsActivity.java
@@ -3,17 +3,26 @@ package com.project.pseudotrade;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Set;
 
 
 public class SettingsActivity extends AppCompatActivity {
+
+    SharedPreferences mSharedPreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +50,16 @@ public class SettingsActivity extends AppCompatActivity {
 
             }
         });
+
+        updateEmail();
+    }
+
+    private void updateEmail() {
+        mSharedPreference = getSharedPreferences("LoginActivityShared", Context.MODE_PRIVATE); //open SharedPreference for read
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//        Log.i("SettingsActivity",user.getEmail());
+//        AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(),"");
     }
 
 
